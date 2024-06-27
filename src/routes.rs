3265d084dce74a14e5bda45537ede64be625641c
Paolo <149602456+pdtfh@users.noreply.github.com>
2024-06-27 12:20:55 +0200

    docker check & health check

--- src/routes.rs
+++ src/routes.rs
@@ -1,7 +1,10 @@
-use aide::axum::{routing::post, ApiRouter};
+use aide::axum::{routing::get, routing::post, ApiRouter};
 
 mod generate_token;
+mod health;
 
 pub fn handler() -> ApiRouter {
-    ApiRouter::new().api_route("/g", post(generate_token::handler))
+    ApiRouter::new()
+        .api_route("/g", post(generate_token::handler))
+        .api_route("/health", get(health::handler))
 }
