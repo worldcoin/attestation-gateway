afbb9564b526de5bfb91b1922f71f06193a5dd2b
andreyudentsovottofeller <andrey.udentsov@ottofeller.com>
2025-03-26 23:16:23 +0100

    Fix test

--- tests/generate_token_integration.rs
+++ tests/generate_token_integration.rs
@@ -81,6 +81,7 @@ fn get_global_config_extension() -> Extension<attestation_gateway::utils::Global
         enabled_bundle_identifiers: vec![BundleIdentifier::AndroidStageWorldApp, BundleIdentifier::AndroidDevWorldApp, BundleIdentifier::IOSStageWorldApp, BundleIdentifier::IOSProdWorldApp],
         log_client_errors: false,
         kinesis_stream_name: None,
+        kinesis_stream_arn: None,
     };
     Extension(config)
 }
@@ -580,6 +581,7 @@ async fn test_server_error_is_properly_logged() {
             enabled_bundle_identifiers: vec![BundleIdentifier::AndroidDevWorldApp],
             log_client_errors: false,
             kinesis_stream_name: None,
+            kinesis_stream_arn: None,
         };
         Extension(config)
     }
