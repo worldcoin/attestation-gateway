4f959838eeff66b929183e34040ae4b81c52f16c
pdtfh <149602456+pdtfh@users.noreply.github.com>
2025-05-02 04:10:15 +0200

    Revert "add ip logging (#81)" (#87)
    This reverts commit a34db01dd7bdd27d13fb83e6dc589546ca170e13.

--- src/routes.rs
+++ src/routes.rs
@@ -4,38 +4,12 @@ use aide::axum::{
     routing::{get, post},
     ApiRouter,
 };
-use axum::{
-    body::Body,
-    extract::ConnectInfo,
-    http::Request,
-    middleware::{self, Next},
-    response::Response,
-};
-use std::net::SocketAddr;
 use tower_http::timeout::TimeoutLayer;
 use tower_http::trace::TraceLayer;
 
 mod generate_token;
 mod health;
 mod jwks;
-
-async fn log_ip_middleware(req: Request<Body>, next: Next) -> Response {
-    let method = req.method().clone();
-    let uri = req.uri().clone();
-
-    let client_ip =
-        if let Some(ConnectInfo(addr)) = req.extensions().get::<ConnectInfo<SocketAddr>>() {
-            addr.to_string()
-        } else {
-            "unknown".to_string()
-        };
-
-    let span = tracing::info_span!("request", "network.client.ip" = %client_ip, method = %method, path = %uri);
-
-    let _guard = span.enter();
-
-    next.run(req).await
-}
 
 #[must_use]
 pub fn get_timeout_layer() -> TimeoutLayer {
@@ -47,7 +21,6 @@ pub fn handler() -> ApiRouter {
         .api_route("/g", post(generate_token::handler))
         .api_route("/.well-known/jwks.json", get(jwks::handler))
         .api_route("/health", get(health::handler))
-        .route_layer(middleware::from_fn(log_ip_middleware))
         .layer(TraceLayer::new_for_http()) // adds HTTP tracing & context to all routes
         .layer(get_timeout_layer())
 }
