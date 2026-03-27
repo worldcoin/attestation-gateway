2b1639e13ee35d10f0e038a1646a173fd1237415
Dmitry Tyshchenko <dmitry.tyshchenko@toolsforhumanity.com>
2026-03-27 18:11:37 +0200

    Fix dead code warnings

--- attestation-gateway/src/android/key_description/key_description_1.rs
+++ attestation-gateway/src/android/key_description/key_description_1.rs
@@ -4,13 +4,13 @@ use crate::android::key_description::unordered_set_of_u64::UnorderedSetOfU64;
 
 #[derive(asn1::Asn1Read)]
 pub struct KeyDescription1<'a> {
-    pub attestation_version: u64,
+    pub _attestation_version: u64,
     pub attestation_security_level: asn1::Enumerated,
     /// Keymaster 1.0 reports schema version `2` here (see Android docs).
-    pub keymaster_version: u64,
+    pub _keymaster_version: u64,
     pub keymaster_security_level: asn1::Enumerated,
     pub attestation_challenge: &'a [u8],
-    pub unique_id: &'a [u8],
+    pub _unique_id: &'a [u8],
     pub software_enforced: AuthorizationList<'a>,
     pub hardware_enforced: AuthorizationList<'a>,
 }
@@ -18,45 +18,45 @@ pub struct KeyDescription1<'a> {
 #[derive(asn1::Asn1Read)]
 pub struct AuthorizationList<'a> {
     #[explicit(1)]
-    pub purpose: Option<asn1::SetOf<'a, u64>>,
+    pub _purpose: Option<asn1::SetOf<'a, u64>>,
     #[explicit(2)]
-    pub algorithm: Option<u64>,
+    pub _algorithm: Option<u64>,
     #[explicit(3)]
-    pub key_size: Option<u64>,
+    pub _key_size: Option<u64>,
     #[explicit(5)]
-    pub digest: Option<UnorderedSetOfU64>,
+    pub _digest: Option<UnorderedSetOfU64>,
     #[explicit(6)]
-    pub padding: Option<asn1::SetOf<'a, u64>>,
+    pub _padding: Option<asn1::SetOf<'a, u64>>,
     #[explicit(10)]
-    pub ec_curve: Option<u64>,
+    pub _ec_curve: Option<u64>,
     #[explicit(200)]
-    pub rsa_public_exponent: Option<u64>,
+    pub _rsa_public_exponent: Option<u64>,
     #[explicit(400)]
-    pub active_date_time: Option<u64>,
+    pub _active_date_time: Option<u64>,
     #[explicit(401)]
-    pub origination_expire_date_time: Option<u64>,
+    pub _origination_expire_date_time: Option<u64>,
     #[explicit(402)]
-    pub usage_expire_date_time: Option<u64>,
+    pub _usage_expire_date_time: Option<u64>,
     #[explicit(503)]
-    pub no_auth_required: Option<asn1::Null>,
+    pub _no_auth_required: Option<asn1::Null>,
     #[explicit(504)]
-    pub user_auth_type: Option<u64>,
+    pub _user_auth_type: Option<u64>,
     #[explicit(505)]
-    pub auth_timeout: Option<u64>,
+    pub _auth_timeout: Option<u64>,
     #[explicit(506)]
-    pub allow_while_on_body: Option<asn1::Null>,
+    pub _allow_while_on_body: Option<asn1::Null>,
     #[explicit(600)]
-    pub all_applications: Option<asn1::Null>,
+    pub _all_applications: Option<asn1::Null>,
     #[explicit(701)]
-    pub creation_date_time: Option<u64>,
+    pub _creation_date_time: Option<u64>,
     #[explicit(702)]
     pub origin: Option<u64>,
     #[explicit(703)]
-    pub rollback_resistant: Option<asn1::Null>,
+    pub _rollback_resistant: Option<asn1::Null>,
     #[explicit(704)]
     pub root_of_trust: Option<RootOfTrust<'a>>,
     #[explicit(705)]
-    pub os_version: Option<u64>,
+    pub _os_version: Option<u64>,
     #[explicit(706)]
     pub os_patch_level: Option<u64>,
 }
@@ -64,7 +64,7 @@ pub struct AuthorizationList<'a> {
 /// Root of trust for attestation schema versions 1 and 2 (no `verified_boot_hash`).
 #[derive(asn1::Asn1Read, Debug)]
 pub struct RootOfTrust<'a> {
-    pub verified_boot_key: &'a [u8],
+    pub _verified_boot_key: &'a [u8],
     pub device_locked: bool,
     pub verified_boot_state: asn1::Enumerated,
 }
--- attestation-gateway/src/android/key_description/key_description_3.rs
+++ attestation-gateway/src/android/key_description/key_description_3.rs
@@ -4,12 +4,12 @@ use crate::android::key_description::unordered_set_of_u64::UnorderedSetOfU64;
 
 #[derive(asn1::Asn1Read)]
 pub struct KeyDescription3<'a> {
-    pub attestation_version: u64,
+    pub _attestation_version: u64,
     pub attestation_security_level: asn1::Enumerated,
-    pub keymaster_version: u64,
+    pub _keymaster_version: u64,
     pub keymaster_security_level: asn1::Enumerated,
     pub attestation_challenge: &'a [u8],
-    pub unique_id: &'a [u8],
+    pub _unique_id: &'a [u8],
     pub software_enforced: AuthorizationList<'a>,
     pub hardware_enforced: AuthorizationList<'a>,
 }
@@ -28,81 +28,81 @@ impl<'a> KeyDescription3<'a> {
 #[derive(asn1::Asn1Read)]
 pub struct AuthorizationList<'a> {
     #[explicit(1)]
-    pub purpose: Option<asn1::SetOf<'a, u64>>,
+    pub _purpose: Option<asn1::SetOf<'a, u64>>,
     #[explicit(2)]
-    pub algorithm: Option<u64>,
+    pub _algorithm: Option<u64>,
     #[explicit(3)]
-    pub key_size: Option<u64>,
+    pub _key_size: Option<u64>,
     #[explicit(4)]
-    pub block_mode: Option<asn1::SetOf<'a, u64>>,
+    pub _block_mode: Option<asn1::SetOf<'a, u64>>,
     #[explicit(5)]
-    pub digest: Option<UnorderedSetOfU64>,
+    pub _digest: Option<UnorderedSetOfU64>,
     #[explicit(6)]
-    pub padding: Option<asn1::SetOf<'a, u64>>,
+    pub _padding: Option<asn1::SetOf<'a, u64>>,
     #[explicit(7)]
-    pub caller_nonce: Option<asn1::Null>,
+    pub _caller_nonce: Option<asn1::Null>,
     #[explicit(8)]
-    pub min_mac_length: Option<u64>,
+    pub _min_mac_length: Option<u64>,
     #[explicit(10)]
-    pub ec_curve: Option<u64>,
+    pub _ec_curve: Option<u64>,
     #[explicit(200)]
-    pub rsa_public_exponent: Option<u64>,
+    pub _rsa_public_exponent: Option<u64>,
     #[explicit(303)]
-    pub rollback_resistance: Option<asn1::Null>,
+    pub _rollback_resistance: Option<asn1::Null>,
     #[explicit(400)]
-    pub active_date_time: Option<u64>,
+    pub _active_date_time: Option<u64>,
     #[explicit(401)]
-    pub origination_expire_date_time: Option<u64>,
+    pub _origination_expire_date_time: Option<u64>,
     #[explicit(402)]
-    pub usage_expire_date_time: Option<u64>,
+    pub _usage_expire_date_time: Option<u64>,
     #[explicit(502)]
-    pub user_secure_id: Option<u64>,
+    pub _user_secure_id: Option<u64>,
     #[explicit(503)]
-    pub no_auth_required: Option<asn1::Null>,
+    pub _no_auth_required: Option<asn1::Null>,
     #[explicit(504)]
-    pub user_auth_type: Option<u64>,
+    pub _user_auth_type: Option<u64>,
     #[explicit(505)]
-    pub auth_timeout: Option<u64>,
+    pub _auth_timeout: Option<u64>,
     #[explicit(506)]
-    pub allow_while_on_body: Option<asn1::Null>,
+    pub _allow_while_on_body: Option<asn1::Null>,
     #[explicit(507)]
-    pub trusted_user_presence_req: Option<asn1::Null>,
+    pub _trusted_user_presence_req: Option<asn1::Null>,
     #[explicit(508)]
-    pub trusted_confirmation_req: Option<asn1::Null>,
+    pub _trusted_confirmation_req: Option<asn1::Null>,
     #[explicit(509)]
-    pub unlocked_device_req: Option<asn1::Null>,
+    pub _unlocked_device_req: Option<asn1::Null>,
     #[explicit(701)]
-    pub creation_date_time: Option<u64>,
+    pub _creation_date_time: Option<u64>,
     #[explicit(702)]
     pub origin: Option<u64>,
     #[explicit(704)]
     pub root_of_trust: Option<RootOfTrust<'a>>,
     #[explicit(705)]
-    pub os_version: Option<u64>,
+    pub _os_version: Option<u64>,
     #[explicit(706)]
     pub os_patch_level: Option<u64>,
     #[explicit(709)]
     pub attestation_application_id: Option<&'a [u8]>,
     #[explicit(710)]
-    pub attestation_id_brand: Option<&'a [u8]>,
+    pub _attestation_id_brand: Option<&'a [u8]>,
     #[explicit(711)]
-    pub attestation_id_device: Option<&'a [u8]>,
+    pub _attestation_id_device: Option<&'a [u8]>,
     #[explicit(712)]
-    pub attestation_id_product: Option<&'a [u8]>,
+    pub _attestation_id_product: Option<&'a [u8]>,
     #[explicit(713)]
-    pub attestation_id_serial: Option<&'a [u8]>,
+    pub _attestation_id_serial: Option<&'a [u8]>,
     #[explicit(714)]
-    pub attestation_id_imei: Option<&'a [u8]>,
+    pub _attestation_id_imei: Option<&'a [u8]>,
     #[explicit(715)]
-    pub attestation_id_meid: Option<&'a [u8]>,
+    pub _attestation_id_meid: Option<&'a [u8]>,
     #[explicit(716)]
-    pub attestation_id_manufacturer: Option<&'a [u8]>,
+    pub _attestation_id_manufacturer: Option<&'a [u8]>,
     #[explicit(717)]
-    pub attestation_id_model: Option<&'a [u8]>,
+    pub _attestation_id_model: Option<&'a [u8]>,
     #[explicit(718)]
-    pub vendor_patch_level: Option<u64>,
+    pub _vendor_patch_level: Option<u64>,
     #[explicit(719)]
-    pub boot_patch_level: Option<u64>,
+    pub _boot_patch_level: Option<u64>,
 }
 
 /// Contents of authorization tag `709` (`attestation_application_id`). See
@@ -117,13 +117,13 @@ pub struct AttestationApplicationId<'a> {
 #[derive(asn1::Asn1Read, Debug, Clone, PartialEq, Eq)]
 pub struct AttestationPackageInfo<'a> {
     pub package_name: &'a [u8],
-    pub version: u64,
+    pub _version: u64,
 }
 
 #[derive(asn1::Asn1Read, Debug)]
 pub struct RootOfTrust<'a> {
-    pub verified_boot_key: &'a [u8],
+    pub _verified_boot_key: &'a [u8],
     pub device_locked: bool,
     pub verified_boot_state: asn1::Enumerated,
-    pub verified_boot_hash: &'a [u8],
+    pub _verified_boot_hash: &'a [u8],
 }
